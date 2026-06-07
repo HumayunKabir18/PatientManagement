52a3cf31c92875c3d894ec2b60a989f8f82f9385
Md Humayun Kabir <md.humayun0kabir@gmail.com>
2026-06-07 23:03:58 +0600

    icon fragmnet and layouts

---
+++ src/main/java/com/niketon/aroggo/app/patient/entity/PatientSearchEntity.java
@@ -0,0 +1,17 @@
+package com.niketon.aroggo.app.patient.entity;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+public class PatientSearchEntity {
+
+	private String patientName;
+	private String mobileNo;
+	private String bloodGroup;
+	private LocalDate dateOfBirth;
+
+	private int page;
+	private int size;
+}
